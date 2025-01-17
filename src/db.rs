ba96fb0f405f342bdf39920f653eab12206668b5
techcoderx <engtianxi@outlook.com>
2025-01-17 14:47:19 +0800

    db query error type

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,20 @@
 use deadpool_postgres::{ Config, CreatePoolError, Manager, ManagerConfig, RecyclingMethod, Runtime };
 use deadpool::managed::Pool;
-use tokio_postgres::{ types::ToSql, Error, NoTls, Row };
+use tokio_postgres::{ types::ToSql, NoTls, Row };
+use std::{ fmt, error };
 use crate::config;
+
+#[derive(Debug)]
+pub struct DbQueryError {
+  message: String,
+}
+
+impl error::Error for DbQueryError {}
+impl fmt::Display for DbQueryError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self.message)
+  }
+}
 
 pub fn init_pool() -> Result<Pool<Manager>, CreatePoolError> {
   let mut cfg = Config::new();
@@ -13,8 +26,15 @@ pub fn init_pool() -> Result<Pool<Manager>, CreatePoolError> {
   Ok(pool)
 }
 
-pub async fn query(pool: Pool<Manager>, statement: &str, params: &[&(dyn ToSql + Sync)]) -> Result<Vec<Row>, Error> {
-  let client = pool.get().await.unwrap();
-  let rows = client.query(statement, params).await?;
-  Ok(rows)
+pub async fn query(pool: Pool<Manager>, statement: &str, params: &[&(dyn ToSql + Sync)]) -> Result<Vec<Row>, DbQueryError> {
+  let client = match pool.get().await {
+    Ok(v) => v,
+    Err(e) => {
+      return Err(DbQueryError { message: e.to_string() });
+    }
+  };
+  match client.query(statement, params).await {
+    Ok(rows) => Ok(rows),
+    Err(e) => Err(DbQueryError { message: e.to_string() }),
+  }
 }
--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,10 @@ use actix_web::{ get, web, HttpResponse, Responder };
 use deadpool_postgres::Manager;
 use deadpool::managed::Pool;
 use serde_json;
+use log;
 use crate::db;
+
+const GENERIC_DB_ERR: &str = "Unknown error occured when querying database";
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -11,26 +14,38 @@ async fn hello() -> impl Responder {
 
 #[get("/languages")]
 async fn list_langs(db_pool: web::Data<Pool<Manager>>) -> impl Responder {
-  let rows = db
-    ::query(
+  let rows = match
+    db::query(
       db_pool.get_ref().clone(),
       "SELECT jsonb_agg(jsonb_build_object('id',id,'name',name)) FROM vsc_cv.languages",
       &[]
     ).await
-    .unwrap();
+  {
+    Ok(r) => r,
+    Err(e) => {
+      log::error!("GET /languages failed: {}", e.to_string());
+      return HttpResponse::BadGateway().json(serde_json::json!({ "error": GENERIC_DB_ERR }));
+    }
+  };
   let result: serde_json::Value = rows[0].get(0);
   HttpResponse::Ok().json(result)
 }
 
 #[get("/licenses")]
 async fn list_licenses(db_pool: web::Data<Pool<Manager>>) -> impl Responder {
-  let rows = db
-    ::query(
+  let rows = match
+    db::query(
       db_pool.get_ref().clone(),
       "SELECT jsonb_agg(jsonb_build_object('id',id,'name',name)) FROM vsc_cv.licenses",
       &[]
     ).await
-    .unwrap();
+  {
+    Ok(r) => r,
+    Err(e) => {
+      log::error!("GET /licenses failed: {}", e.to_string());
+      return HttpResponse::BadGateway().json(serde_json::json!({ "error": GENERIC_DB_ERR }));
+    }
+  };
   let result: serde_json::Value = rows[0].get(0);
   HttpResponse::Ok().json(result)
 }
